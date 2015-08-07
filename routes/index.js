435ad87ddb6ed72d0c7e3f93ce26426b2b519bfa
ARP <arpgithub@gmail.com>
2015-08-07 22:16:21 +0200

    M7: Autoload. Correccion errores en answer.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,21 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
 
-/* GET creditos page */
+// GET creditos page 
 router.get('/author', function(req, res) {
   res.render('author', {});
 });
+
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
 
 // Definición de rutas de quizes
