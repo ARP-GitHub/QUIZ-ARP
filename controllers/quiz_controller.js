c6cbdf55acb11bdc30c3279d0d8ffe591322145c
ARP <arpgithub@gmail.com>
2015-08-09 17:07:27 +0200

    M9: Creación comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,8 +2,12 @@
 
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-   models.Quiz.findById(quizId).then(
-      function(quiz) {
+   //models.Quiz.findById({
+   models.Quiz.find({
+		where: { id: Number(quizId) },
+		include: [{ model: models.Comment }]
+	     }
+   ).then(function(quiz){
          if (quiz) {
             req.quiz = quiz;
             next();
